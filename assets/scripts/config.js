af51c57470470465213321daffa27550808c3c41
TedCart <TedCart@gmail.com>
2018-03-06 11:08:50 -0500

    Added frontOrigins to config.js
    Now can reference current domain for callback URIs to spotify, similar to how we set the API during AJAX calls.

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -4,6 +4,10 @@ const config = {
   apiOrigins: {
     development: 'http://localhost:4741',
     production: 'https://spotify-assistant-itunes.herokuapp.com/'
+  },
+  frontOrigins: {
+    development: 'http://localhost:7165',
+    production: 'https://tedcart.github.io/spotify-assistant'
   }
 }
 
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -10,5 +10,9 @@ $(() => {
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
+$(() => {
+  // setAPIOrigin(location, config)
+})
+
 // use require without a reference to ensure a file is bundled
 // require('./example')
